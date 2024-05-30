74ef328a84d16c36a0d9e93250e6e7f497a0d1d1
qr-oh <qiren14@gmail.com>
2024-05-30 16:11:02 +0800

    Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('test')
+// Define HTML elements
+
+const board = document.getElementById("game-board");
+
+// Define game variables
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+    drawFood();
+}
+
+// Draw snake
+function drawSnake() {
+
+}
